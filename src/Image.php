68d7d21e1cf5ab817c97ffe2e3e81b4b8cedb2ae
Walter Luis <walterluisglez@gmail.com>
2022-07-15 00:08:14 +0100

    Class created with Image enhanced features "\WalterLuis\Utils\Image".

---
+++ src/Image.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WalterLuis\Utils;
+
+/**
+ * Image enhanced features.
+ *
+ * @copyright Copyright © 2022 Walter Luis
+ * @license   MIT
+ * @author    Walter Luis <walterluisglez@gmail.com>
+ */
+class Image
+{
+}
